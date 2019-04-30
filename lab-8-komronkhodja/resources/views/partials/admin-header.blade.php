25720e85e78bd23365dea8bbddb34263cbdad2cb
komronkhodja <33807608+komronkhodja@users.noreply.github.com>
2019-04-30 18:13:28 +0500

    u1710234

---
+++ lab-8-komronkhodja/resources/views/partials/admin-header.blade.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a href="{ route('blog.index')}" class="navbar-brand">Laravel Guide</a>
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="{{ route('admin.index') }}">Posts</a></li>     
+
+            </ul>
+        
+        </div>
+
+    </div>
+</nav>
+
+
+
